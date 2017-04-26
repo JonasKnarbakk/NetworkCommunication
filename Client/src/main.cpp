2dd4d3c696016d7bb5b371e039ebc7086b6bb200
Jonas Knarbakk <jonas@knarbakk.no>
2017-04-26 22:39:33 +0200

    Initial commit

---
+++ Client/src/main.cpp
@@ -0,0 +1,11 @@
+// Description: New project
+// Author: Jonas Knarbakk (20:18:36-26/04/2017)
+
+#include <cstdio>
+
+int main( int argc, char ** argv ){
+
+    puts("Hello World!");
+    return 0;
+
+}
