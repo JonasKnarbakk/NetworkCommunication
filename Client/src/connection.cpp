daec434b732bfe0cb5a2709c2c2b22cbfe39ce8c
Jonas Knarbakk <jonas@knarbakk.no>
2017-05-02 15:33:25 +0200

    Seperated the connection code into its own class

---
+++ Client/src/connection.cpp
@@ -0,0 +1,78 @@
+#include "connection.hpp"
+
+void error(const char *msg){
+    perror(msg);
+    exit(1);
+}
+
+Connection::Connection(std::string serv_addr, unsigned int port){
+
+    if(port < 1024){
+
+        error("Port number given is not a valid port number.\nA valid port numbers is above the reserved system ports, ranging from 0-1023.\n");
+
+    }
+
+    tcp_socket = socket(AF_INET, SOCK_STREAM, 0); /* Open the socket */
+
+    if(tcp_socket < 0){
+
+        error("ERROR: could not open socket!\n");
+    
+    }
+
+    server = gethostbyname(serv_addr.c_str());
+
+    if( server == NULL ){
+    
+        error("ERROR: no such host!\n");
+    
+    }
+
+    memset(&server_address, 0, sizeof(server_address));
+
+    server_address.sin_family = AF_INET; /* AF_INET is requires */
+    memcpy(server->h_addr, &server_address.sin_addr.s_addr, server->h_length); /* Copy over the server adress */
+    server_address.sin_port = htons(port); /* Set the port number */
+}
+
+void Connection::sendToServer(const char *message){
+
+    if(connect(tcp_socket, (const sockaddr*) &server_address, sizeof(server_address)) < 0){
+    
+        error("ERROR: couldn't connect to the server\n");
+    
+    }
+
+    // Zero out the buffer
+    memset(buffer, 0, sizeof(buffer));
+
+    // Copy the message into the buffer
+    memcpy(buffer, message, strlen(message));
+
+    std::cout << "Text to send (max 255 characters): ";
+
+    // Get input from the user
+    std::cin.getline(buffer, 255);
+
+    // Send data to the server
+    read_write_size = write(tcp_socket, buffer, 255);
+    
+    if(read_write_size < 0){
+        error("ERROR: couldn't write to socket\n");
+    }
+
+    // Zero out the buffer
+    memset(buffer, 0, sizeof(buffer));
+
+    // Read the response from the server
+    read_write_size = read(tcp_socket, buffer, 255);
+
+    if(read_write_size < 0){
+        error("ERROR: couldn't read from socket\n");
+    }
+
+    std::cout << buffer << std::endl;
+
+    close(tcp_socket);
+}
---
+++ Client/src/connection.hpp
@@ -0,0 +1,28 @@
+#ifndef CONNECTION_HPP
+#define CONNECTION_HPP
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <cstdlib>
+#include <iostream>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <cstring>
+class Connection{
+    public:
+        Connection(std::string server_address, unsigned int port_number);
+
+        void sendToServer(const char *message);
+    private:
+        int tcp_socket,
+            read_write_size;
+
+        char buffer[256];
+
+        struct sockaddr_in server_address;
+        struct hostent *server;
+};
+
+#endif
